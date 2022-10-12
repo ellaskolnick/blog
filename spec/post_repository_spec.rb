2ee26ccad8a305b0706e00f45bcef0aee804cbc5
Ella Skolnick <ellaskolnick@me.com>
2022-10-12 16:38:55 +0100

    addition to tests

--- spec/post_repository_spec.rb
+++ spec/post_repository_spec.rb
@@ -1,6 +1,16 @@
 require 'post_repository'
 
+def reset_posts_table
+  seed_sql = File.read('spec/seeds_comments.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'blog_test' })
+  connection.exec(seed_sql)
+end
+
 RSpec.describe PostRepository do
+  before(:each) do
+    reset_posts_table
+  end
+
   it "finds one post with comments" do
     repo = PostRepository.new
     post = repo.find_with_comments(1)
